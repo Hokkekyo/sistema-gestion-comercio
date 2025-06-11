37ab70da695b4b9677ba027bf9cda2ca0416f0c8
Luciano Alvarez <138730789+Hokkekyo@users.noreply.github.com>
2025-06-11 16:33:46 -0300

    update comentarios funciones.cpp

--- src/funciones.cpp
+++ src/funciones.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include "../include/funciones.h"
+#include <cctype>
+#include <iostream>
 
 using namespace std;
 
@@ -7,85 +8,84 @@ using namespace std;
  * Archivo: funciones.cpp
  * Acá va casi todo el trabajo pesado.
  *
- * Acá se implementan las funciones que usamos para cargar datos, validar, mostrar reportes, etc.
- * La idea es que todo lo que haga cosas (lógica del sistema) esté acá, así el main no se llena de cosas.
+ * Acá se implementan las funciones que usamos para cargar datos, validar,
+ * mostrar reportes, etc. La idea es que todo lo que haga cosas (lógica del
+ * sistema) esté acá, así el main no se llena de cosas.
  */
 
-//vCodigomarca,vNombremarca,cantidadMarcasCargadas,marcasCargadas
+// ===================================================
+// ===========  CARGA DE MARCAS - LOTE 1 =============
+// ===================================================
+// vCodigomarca,vNombremarca,cantidadMarcasCargadas,marcasCargadas
+void cargarLoteMarcas(int vCodigomarca[], string vNombremarca[], int &cantidadMarcasCargadas, bool &marcasCargadas) {
+    cantidadMarcasCargadas = 0;
+    marcasCargadas = false;
 
-void cargarLoteMarcas(int vCodigomarca[], string vNombremarca[], int &cantidadMarcasCargadas, bool &marcasCargadas)
-{
-cantidadMarcasCargadas=0;
-marcasCargadas=false;
+    int codigoIngresado;
+    string nombreIngresado;
+    bool marcaValida;
+    
+    cout << "\n –––––––––––––––––––––––––––––––––––––––––––––––––––––" << endl;
+    cout << " INICIANDO CARGA DE LOTE 1 - Código y nombre de marcas." << endl;
+    cout << " –––––––––––––––––––––––––––––––––––––––––––––––––––––" << endl << endl;
 
-int codigoIngresado;
-string nombreIngresado;
-bool marcaValida;
+    for (int i = 0; i < 10; i++) {
+        cout << "A continuación, ingrese los datos para la marca #" << i + 1 << "." << endl;
 
-cout << "\n –––––––––––––––––––––––––––––––––––––––––––––––––––––" << endl;
-cout << " INICIANDO CARGA DE LOTE 1 - Código y nombre de marcas."<<endl;
-cout << " –––––––––––––––––––––––––––––––––––––––––––––––––––––" << endl;
-cout<<endl;
-
-for (int i=0; i<10; i++){
-    cout<<"A continuación, ingrese los datos para la Marca #"<<i+1<<"."<<endl;
-    //cout<<endl;
-
-    do{
-        marcaValida=false;
-        cout<<"- Código de marca (número entero del 1 al 10): ";
-        cin>>codigoIngresado;
+        // Solicitamos ingresar código de marca:
+        do {
+            marcaValida = false;
+            cout << "- Código de marca (número entero del 1 al 10): ";
+            cin >> codigoIngresado;
         
-        //Se verifica que el codigo ingresado sea entre 1 y 10
-        if (codigoIngresado>=1 && codigoIngresado<=10){
-            marcaValida=true;
-        }
-        else {
-            cout<<"\nEl código ingresado no es válido. Intente nuevamente."<<endl;
-        }
-    } while (marcaValida==false);
+            // Se verifica que el codigo ingresado sea entre 1 y 10
+            if (codigoIngresado >= 1 && codigoIngresado <= 10) {
+                marcaValida = true;
+            } else {
+                cout << "\nEl código ingresado no es válido. Intente nuevamente." << endl;
+            }
+        } while (marcaValida == false);
 
-//Si el codigo es correcto se guarda en el vector 
-vCodigomarca[i]= codigoIngresado;
+        vCodigomarca[i] = codigoIngresado; // Si el codigo es correcto se guarda en el vector
 
-cin.ignore(); // Limpia el buffer antes de getline
+        cin.ignore(); // Limpia el buffer antes de getline
 
-do {
-    cout<<"- Nombre de marca (no puede estar vacío): ";
-    getline(cin, nombreIngresado); //Es para evitar que ingresen solo espacios 
-    cout<<endl;
+        // Solicitamos ingresar nombre de marca:
+            do {
+                cout << "- Nombre de marca (no puede estar vacío): ";
+                getline(cin, nombreIngresado); // Es para evitar que ingresen solo espacios
+                cout << endl;
+        
+                nombreIngresado.erase(0, nombreIngresado.find_first_not_of(' '));
+                nombreIngresado.erase(nombreIngresado.find_last_not_of(' ') + 1);
+        
+                marcaValida = false;
+                if (!nombreIngresado.empty()) {
+                    marcaValida = true;
+                } else {
+                    cout << "El nombre ingresado no es válido. Intente nuevamente." << endl;
+                }
+            } while (marcaValida == false);
 
-    nombreIngresado.erase(0, nombreIngresado.find_first_not_of(' '));
-    nombreIngresado.erase(nombreIngresado.find_last_not_of(' ')+1);
-
-    marcaValida=false;
-    if (!nombreIngresado.empty()){
-        marcaValida=true;
-    } 
-    else {
-        cout<<"El nombre ingresado no es válido. Intente nuevamente."<<endl;
+            vNombremarca[i] = nombreIngresado;
+            cantidadMarcasCargadas++; // Usamos este contador para contar la cantidad
+                                      // de marcas que se cargan correctamente
     }
-} while (marcaValida==false);
 
-vNombremarca[i]= nombreIngresado;
+    // En este if validamos que la carga se completó.
+    if (cantidadMarcasCargadas == 10) {
+        marcasCargadas = true;
+        cout << "Carga del lote 1 completada con éxito. A continuación se muestra el listado:" << endl << endl;
 
-cantidadMarcasCargadas ++;//Usamos este contador para contar la cantidad 
-//de marcas que se cargan correctamente 
-}
-//En este if validamos que la carga se completo y en el for mostramos el listado de marcas cargadas
-
-if (cantidadMarcasCargadas==10){
-    marcasCargadas=true;
-    cout<<"Carga del lote 1 completada con éxito. A continuación se muestra el listado:"<<endl << endl;
-
-    for (int x=0; x<10; x++){
-    cout<<"Código de marca: "<<vCodigomarca[x]<<"   |   Nombre de marca: "<<vNombremarca[x]<<endl;
-}
-}
-else {
-    cout<<"No se completo correctamente la carga del lote 1."<<endl;
+        // Mostramos la carga de datos:
+        for (int x = 0; x < 10; x++) {
+            cout << "Código de marca: " << vCodigomarca[x] << "   |   Nombre de marca: " << vNombremarca[x] << endl;
+        }
+    } else {
+        cout << "No se completo correctamente la carga del lote 1." << endl;
+    }
 }
 
- }
-
- 
+// ===================================================
+// ===========  CARGA DE MARCAS - LOTE 2 =============
+// ===================================================
