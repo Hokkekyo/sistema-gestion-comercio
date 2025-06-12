f9de83206633393ed22e12f45992ad68a765634a
Luciano Alvarez <138730789+Hokkekyo@users.noreply.github.com>
2025-06-11 22:31:43 -0300

    minor fixes

--- include/funciones.h
+++ include/funciones.h
@@ -9,10 +9,11 @@ using namespace std;
  * Acá van solo las declaraciones de funciones.
  *
  * Es como un índice que le avisa al main qué funciones puede usar de
- * funciones.cpp. Sirve para mantener organizado el proyecto y separar bien las partes.
+ * funciones.cpp. Sirve para mantener organizado el proyecto y separar bien las
+ * partes.
  */
 
-void cargarLoteMarcas(int vCodigoMarca[], string vNombremarca[],int &cantidadMarcasCargadas, bool &marcasCargadas);
+void cargarLoteMarcas(int vCodigoMarca[], string vNombremarca[], int &cantidadMarcasCargadas, bool &marcasCargadas);
 
 void menuPrincipal(int &opcion);
 
