66f530cd3f26e4e3d7a4ae0ee59613a25afb660a
Luciano Alvarez <138730789+Hokkekyo@users.noreply.github.com>
2025-06-12 14:32:18 -0300

    minor changes

--- src/tempCodeRunnerFile.cpp
+++ src/tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-          // Para regresar al menú de reportes:
-          cout << "\nToca ENTER para ir al menú de reportes.";
-          cin.ignore();
-          cin.get();
-          opInvalida = true;
-          menuPrincipal(opcion);
+ 
